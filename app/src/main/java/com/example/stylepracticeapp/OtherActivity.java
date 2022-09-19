894224719aadd9ebe48c32f4a2c9a394a20e2dec
ddrewi <andrew.mdi130@gmail.com>
2022-09-19 01:25:35 -0500

    added ability to switch screens, linear layout

--- app/src/main/java/com/example/stylepracticeapp/OtherActivity.java
+++ app/src/main/java/com/example/stylepracticeapp/OtherActivity.java
@@ -2,7 +2,9 @@ package com.example.stylepracticeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
 
